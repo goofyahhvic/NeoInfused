f9c3d635b874887d223bf004c498d6bcb0517aba
goofyahhvic <viniciusvmoranimations@gmail.com>
2024-07-06 23:12:16 -0300

    improved project structure

--- NeoInfused-Premake.lua
+++ NeoInfused-Premake.lua
@@ -1,13 +1,9 @@
-OUTPUT_DIR = "%{cfg.buildcfg}-%{cfg.system}"
 INCLUDE_DIR = {}
 LIB_DIR = {}
 
-INCLUDE_DIR["external"] = "external/include/"
-LIB_DIR["external"] = "external/lib/"
-
-INCLUDE_DIR["submods"] = "submods/"
-
-include "glad/glad-Premake.lua"
+include "dependencies/glad/glad-Premake.lua"
+include "dependencies/GLFW-Premake.lua"
+INCLUDE_DIR["glm"] = "dependencies/glm/include/"
 
 project "NeoInfused"
     location "./"
@@ -19,26 +15,23 @@ project "NeoInfused"
     cppdialect "C++20"
     systemversion "latest"
 
-    targetdir "bin/%{OUTPUT_DIR}/%{prj.name}/"
-    objdir "bin-int/%{OUTPUT_DIR}/%{prj.name}/"
-
     pchheader "neo_pch.hpp"
     pchsource "src/neo_pch.cpp"
 
     files {
         "include/**.hpp",
         "src/**.hpp",
-        "src/**.cpp",
+        "src/**.cpp"
     }
     includedirs {
         "include/",
-        "%{INCLUDE_DIR.external}",
-        "%{INCLUDE_DIR.submods}",
-        "%{INCLUDE_DIR.glad}"
+        "%{INCLUDE_DIR.GLFW}",
+        "%{INCLUDE_DIR.glad}",
+        "%{INCLUDE_DIR.glm}"
     }
 
     links {
-        "glfw",
+        "GLFW",
         "glad"
     }
 
@@ -46,15 +39,22 @@ project "NeoInfused"
         toolset "gcc"
         defines { "NEO_PLATFORM_LINUX" }
 
+        links {
+			"Xcursor",
+			"Xinerama",
+			"Xrandr",
+			"Xi",
+			"dl",
+			"X11",
+			"m"
+		}
+
     filter "system:windows"
         toolset "msc"
         defines {
             "NEO_PLATFORM_WINDOWS",
             "_CRT_SECURE_NO_WARNINGS"
         }
-
-        includedirs { "%{INCLUDE_DIR.external}windows/" }
-        libdirs { "%{LIB_DIR.external}windows/" }
     
     filter "configurations:dbg"
         symbols "On"
