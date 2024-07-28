373bcbc43695e8dc03e3948a9ee0bc9a4f09ed59
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2024-07-28 14:38:44 -0300

    Internally remade WindowStorage class
    Internally remade WindowStorage class, now not using std::vector anymore, for adding custom iterators to skip null Window pointers
    Made WindowStorage not use uint64_t for window id's since window id's stopped being uint64_t since last commit

--- include/NeoInfused/core/neo_window-storage.hpp
+++ include/NeoInfused/core/neo_window-storage.hpp
@@ -4,51 +4,58 @@
 #include "neo_window.hpp"
 
 namespace neo {
+	class WindowStorage_iterator {
+		using _ThisT = WindowStorage_iterator;
+	public:
+		WindowStorage_iterator(const _ThisT& other) = default;
+		WindowStorage_iterator(_ThisT&& other) = default;
+
+		inline _ThisT& operator++(void) { do m_Ptr++; while (!*m_Ptr); return *this; }
+		inline _ThisT  operator++(int) { _ThisT temp(m_Ptr); do { m_Ptr++; } while (!*m_Ptr); return temp; }
+
+		inline _ThisT& operator--(void) { do { m_Ptr--; } while (!*m_Ptr); return *this; }
+		inline _ThisT  operator--(int) { _ThisT temp(m_Ptr); do { m_Ptr--; } while (!*m_Ptr); return temp; }
+
+		inline Window** operator->() const { return m_Ptr; }
+		inline Window* operator*() const { return *m_Ptr; }
+
+		inline bool operator==(const _ThisT& other) const { return (m_Ptr == other.m_Ptr); }
+		inline auto operator<=>(const _ThisT& other) const = default;
+
+		WindowStorage_iterator(Window** ptr);
+	private:
+		Window** m_Ptr;
+	};
+
 	class WindowStorage {
 	public:
-		using iterator = std::vector<Window*>::iterator;
-		using reverse_iterator = std::vector<Window*>::reverse_iterator;
-		using const_iterator = std::vector<Window*>::const_iterator;
-		using const_reverse_iterator = std::vector<Window*>::const_reverse_iterator;
+		using iterator = WindowStorage_iterator;
 	public:
 		WindowStorage(void);
 		~WindowStorage(void);
 		static inline WindowStorage* Get(void) { return s_This; }
 
-		uint64_t create_window(uint32_t width, uint32_t height, const std::string& title = Core::Get()->exec_name());
-		void destroy_window(uint64_t id);
+		uint32_t create_window(uint32_t width, uint32_t height, const std::string& title = Core::Get()->exec_name());
+		void destroy_window(uint32_t id);
 
 		// destroys all windows and remove clear the container
 		void clear(void);
 		// returns the number of living windows in the container
-		inline size_t size(void) const { return m_Size; }
+		inline size_t size(void) const { return m_WindowsNum; }
 		// returns false if theres more than one living window in the container
-		inline bool empty(void) const { return !m_Size; }
+		inline bool empty(void) const { return !m_WindowsNum; }
 
-		Window* at(uint64_t id);
-		inline Window* operator[](uint64_t id) { return this->at(id); }
+		Window* at(uint32_t id);
+		inline Window* operator[](uint32_t id) { return this->at(id); }
 		Window* main_window(void) { return this->at(main_window_id); }
 
-		inline iterator begin(void) { return m_Windows.begin(); }
-		inline const_iterator begin(void) const { return m_Windows.begin(); }
-		inline const_iterator cbegin(void) const { return m_Windows.begin(); }
-
-		inline iterator end(void) { return m_Windows.end(); }
-		inline const_iterator end(void) const { return m_Windows.end(); }
-		inline const_iterator cend(void) const { return m_Windows.end(); }
-		
-		inline reverse_iterator rbegin(void) { return m_Windows.rbegin(); }
-		inline const_reverse_iterator rbegin(void) const { return m_Windows.rbegin(); }
-		inline const_reverse_iterator crbegin(void) const { return m_Windows.rbegin(); }
-
-		inline reverse_iterator rend(void) { return m_Windows.rend(); }
-		inline const_reverse_iterator rend(void) const { return m_Windows.rend(); }
-		inline const_reverse_iterator crend(void) const { return m_Windows.rend(); }
+		inline iterator begin(void) { return m_Windows; }
+		inline iterator end(void) { return m_Windows + m_Size; }
 	public:
-		uint64_t main_window_id;
+		uint32_t main_window_id;
 	private:
-		std::vector<Window*> m_Windows;
-		size_t m_Size;
+		uint32_t m_WindowsNum, m_Size, m_Capacity;
+		Window** m_Windows;
 		static inline WindowStorage* s_This = nullptr;
 	};
 }
--- src/NeoInfused/core/neo_window-storage.cpp
+++ src/NeoInfused/core/neo_window-storage.cpp
@@ -2,40 +2,51 @@
 #include "NeoInfused/core/neo_window-storage.hpp"
 
 namespace neo {
+	WindowStorage_iterator::WindowStorage_iterator(Window** ptr)
+	: m_Ptr(ptr) {
+		while (!*m_Ptr)
+			m_Ptr++;
+	}
+
 	WindowStorage::WindowStorage(void)
-	: main_window_id(0), m_Size(0) { //, m_IdOffset((uint64_t)rand() + ((uint64_t)rand() << (uint64_t)(rand() % 5)) + (uint64_t)(rand() << 8) + 1) {
+	: main_window_id(0), m_Size(0), m_WindowsNum(0), m_Capacity(32),
+	m_Windows((Window**)malloc(m_Capacity)) {
 		NEO_ASSERT(!s_This, "Cannot create multiple instances of WindowStorage!");
 		s_This = this;
+		m_Windows[0] = (Window*)0xffffffff;
 	}
 	WindowStorage::~WindowStorage(void) {
-		for (size_t i = 0; i < m_Windows.size(); i++)
+		for (uint32_t i = 0; i < m_Size; i++)
 			delete m_Windows[i];
 	}
 	void WindowStorage::clear(void) {
-		for (size_t i = 0; i < m_Windows.size(); i++)
+		for (uint32_t i = 0; i < m_Size; i++)
 			delete m_Windows[i];
-		m_Windows.clear();
 		m_Size = 0;
+		m_WindowsNum = 0;
 	}
 
-	uint64_t WindowStorage::create_window(uint32_t width, uint32_t height, const std::string& title) {
-		if (!m_Size) {
-			main_window_id = m_Windows.size();
-			m_Windows.push_back(new Window(main_window_id, width, height, title));
-		} else {
-			m_Windows.push_back(new Window(m_Windows.size(), width, height, title));
+	uint32_t WindowStorage::create_window(uint32_t width, uint32_t height, const std::string& title) {
+		if (m_Size == m_Capacity) {
+			m_Capacity += 32;
+			m_Windows = (Window**)realloc(m_Windows, m_Capacity);
 		}
-		m_Size++;
-		return m_Windows.size() - 1;
+		if (!m_Size) 
+			main_window_id = m_Size;
+		
+		m_Windows[m_Size++] = new Window(m_Size, width, height, title);
+		m_Windows[m_Size] = (Window*)INTPTR_MAX;
+		m_WindowsNum++;
+		return m_Size - 1;
 	}
-	void WindowStorage::destroy_window(uint64_t id) {
-		m_Size--;
-		if (main_window_id == id)
-			main_window_id = 0;
+	void WindowStorage::destroy_window(uint32_t id) {
+		m_WindowsNum--;
+		if (id == m_Size - 1)
+			m_Size--;
 		delete m_Windows[id];
 		m_Windows[id] = nullptr;
 	}
-	Window* WindowStorage::at(uint64_t id) {
+	Window* WindowStorage::at(uint32_t id) {
 		return m_Windows[id];
 	}
 }
